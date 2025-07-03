7917b17b69c7f8a47469b5fe10b6a40b2efe7c63
Jamil Gonzalez <jamil.gonzalez@icloud.com>
2025-07-02 22:10:36 -0400

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,17 +5,14 @@ const isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
-    const user = await auth.protect()
-    if (!user.isAuthenticated) {
-      return NextResponse.redirect(new URL('/', req.url))
-    }
+    await auth.protect()
   }
 })
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|favicon.ico)).*)',
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
